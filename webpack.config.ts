22f08ee5bc150310ca7609f687aa49952c58a53c
lokeq <lokeq000@gmail.com>
2024-01-29 16:53:56 +0300

    fix webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,10 +1,10 @@
 import webpack from 'webpack';
 import path from 'path';
 import { buildWebpackConfig } from './config/build/buildWebpackConfig';
-import { BuildEnv, BuildPaths } from './config/build/types/config';
+import { BuildEnv, BuildPath } from './config/build/types/config';
 
 export default (env: BuildEnv) => {
-  const paths: BuildPaths = {
+  const paths: BuildPath = {
     entry: path.resolve(__dirname, 'src', 'index.tsx'),
     build: path.resolve(__dirname, 'build'),
     html: path.resolve(__dirname, 'public', 'index.html'),
